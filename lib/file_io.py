8b9f2c71ff3d10525736ca231d53881c37ec5bc2
James Hawk <jph94880@gmail.com>
2023-08-13 20:57:55 -0500

    Adds write_file() function

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,6 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name + '.txt', mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
     pass
